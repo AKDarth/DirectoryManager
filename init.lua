77a1126cc21cb7d5c475c83117ed22b5742cde4f
AKDarth <76193842+AKDarth@users.noreply.github.com>
2020-12-23 18:34:22 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -38,12 +38,8 @@ local Error = require(ReplicatedStorage:WaitForChild('Shared').Utilities.Error);
 
 local TableWrapper = TableExtender._TableWrapper;
 local function ReturnCompatability(Table, IteratorFlag) -- ReturnCompatability (Table: table, IteratorFlag: boolean)
-	Table = ( type(Table) == 'table' and Table ) or Error.With(): InvalidArgument():At {
-		Line = 41, Function = 'ReturnCompatability', ValueName = tostring(Table),
-	};
-	local _ = ( type(IteratorFlag) == 'boolean' ) or Error.With(): InvalidArgument():At {
-		Line = 42, Function = 'ReturnCompatability', ValueName = tostring(IteratorFlag),
-	};
+	Table = ( type(Table) == 'table' and Table ) or Error.With(): InvalidArgument():At {Line = 41, Function = 'ReturnCompatability', ValueName = tostring(Table),};
+	local _ = ( type(IteratorFlag) == 'boolean' ) or Error.With(): InvalidArgument():At {Line = 42, Function = 'ReturnCompatability', ValueName = tostring(IteratorFlag),};
 
 	if (IteratorFlag) then
 		return ( Table[1] and ipairs ) or ( pairs(Table) and pairs );
@@ -53,12 +49,8 @@ local function ReturnCompatability(Table, IteratorFlag) -- ReturnCompatability (
 end;
 
 local function SerializePathArguments(Table) -- SerializePathArguments (Table: table)
-	Table = ( ReturnCompatability(Table, false) == 'ipairs' and Table ) or Error.With(): InvalidArgument():At {
-		Line = 52, Function = 'SerializePathArguments', ValueName = ReturnCompatability(Table, false),
-	};
-	Table = ( #Table ~= 0 and Table) or Error.With(): InvalidArgument():At {
-		Line = 53, Function = 'SerializePathArguments', ValueName = tostring(Table),
-	};
+	Table = ( ReturnCompatability(Table, false) == 'ipairs' and Table ) or Error.With(): InvalidArgument():At {Line = 52, Function = 'SerializePathArguments', ValueName = ReturnCompatability(Table, false),};
+	Table = ( #Table ~= 0 and Table) or Error.With(): InvalidArgument():At {Line = 53, Function = 'SerializePathArguments', ValueName = tostring(Table),};
 
 	local ParsedData, Path = {}, nil;
 	for Index, Component in ipairs(Table) do
@@ -89,27 +81,30 @@ local DirectoryManager = {
 DirectoryManager.ClassName = 'Directory';
 
 function DirectoryManager.PathSearchAsync(Information) -- .PathSearchAsync (Information: table)
-	Information = ( type(Information) == 'string' and Information ) or Error.With(): InvalidArgument():At {
-		Line = 95, Function = '.PathSearchAsync', ValueName = type(Information),
-	};
+	Information = ( type(Information) == 'string' and Information ) or Error.With(): InvalidArgument():At {Line = 95, Function = '.PathSearchAsync', ValueName = type(Information),};
 	
 	local SerializedData = SerializePathArguments (Information);
 	SerializedData = SerializedData and TableWrapper.New(SerializedData);
 	
-	for _, Component in ipairs(Environments[SerializedData.Environment]) do
+	local CachedEnvironment = SerializedData.DeepSearch ('Environment');
+	if ( CachedEnvironment == 'ReplicatedStorage' ) then
+		for _, Component in ipairs(Environments[SerializedData.Environment]) do
+
+		end;
+	else
+		local Getter = DirectoryManager._InternalGetters[CachedEnvironment] or Error.With(): InvalidArgument():At {
+			Line = 104, Function = '.PathSearchAsync', ValueName = SerializedData.Environment
+		};
 		
+		for Index, Component in pairs(Getter) do
+			
+		end;
 	end;
 end;
 
 function DirectoryManager.Init(RequestedEnvironment) -- .Init (RequestedEnvironment: string)
-	RequestedEnvironment = ( type(RequestedEnvironment) == 'string' and RequestedEnvironment ) or Error.With(): InvalidArgument():At {
-		Line = 103, Function = '.Init', ValueName = type(RequestedEnvironment),
-	};
-	
-	RequestedEnvironment = Environments[RequestedEnvironment] or Error.With(): InvalidArgument():At {
-		Line = 107, Function = '.Init', ValueName = RequestedEnvironment,
-	};
-	
+	RequestedEnvironment = ( type(RequestedEnvironment) == 'string' and RequestedEnvironment ) or Error.With(): InvalidArgument():At {Line = 103, Function = '.Init', ValueName = type(RequestedEnvironment),};
+	RequestedEnvironment = Environments[RequestedEnvironment] or Error.With(): InvalidArgument():At {Line = 107, Function = '.Init', ValueName = RequestedEnvironment,};
 	
 	for _, Pathway in ipairs(RequestedEnvironment:GetDescendants()) do
 		
@@ -117,9 +112,7 @@ function DirectoryManager.Init(RequestedEnvironment) -- .Init (RequestedEnvironm
 end;
 
 function DirectoryManager.SafeLoadComponent(Component) -- .SafeLoadComponent (Component: userdata)
-	Component = ( type(Component) == 'userdata' and Component ) or Error.With(): InvalidArgument():At {
-		Line = 120, Function = '.SafeLoadComponent', ValueName = type(Component),
-	};
+	Component = ( type(Component) == 'userdata' and Component ) or Error.With(): InvalidArgument():At {Line = 120, Function = '.SafeLoadComponent', ValueName = type(Component),};
 end;
 
 return DirectoryManager;

aef863ff246f03081e610b1cf18b19afca8a8a2c
AKDarth <76193842+AKDarth@users.noreply.github.com>
2021-01-02 13:10:04 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 --[[
-
 	----------------------------------------------------------
 	    	  Directory (Initializing and Searching)
 	----------------------------------------------------------
@@ -8,7 +7,7 @@
 	
 	Searching:
 	
-		local Domains, Gojo = DirectoryManager.PathSearchAsync('Shared', 'Domains', 'Gojo');
+		local Domains, Gojo = DirectoryManager.SearchPath('Shared', 'Domains', 'Gojo');
 		
 	Initializing:
 	
@@ -17,9 +16,7 @@
 	
 			return SystemsDirectory.Init(Environment);
 		end;
-
 		local Worked, PackedEnvironment = InitializeServer('Server');
-
 --]]
 
 -- // Services / Modules
@@ -43,50 +40,49 @@ local DirectoryManager = {
 }
 DirectoryManager.ClassName = 'DirectoryManager'
 
-function DirectoryManager.PathSearchAsync(requestedEnvironment, ...)
+function DirectoryManager.SearchPath(requestedEnvironment, ...)
 	requestedEnvironment = ( type(requestedEnvironment) == 'string' and requestedEnvironment ) or error('Environment argument must be a string')
 	local packedArguments, buffer = {...}, {}
-	
+
 	local cachedEnvironment = environments[requestedEnvironment] or error('Requested environment does not exist')
 	if DirectoryManager._hasNotInitialized[cachedEnvironment] then
 		isYielding = isYielding == false
 		repeat runServiceHeartbeat:Wait() until not isYielding
 	end
-	
+
 	for _, componentOne in ipairs(packedArguments) do
-		if DirectoryManager._internalGetters[componentOne] then
-			buffer[#buffer + 1] = componentOne
-		end
+		buffer[#buffer + 1] = DirectoryManager._internalGetters[requestedEnvironment][componentOne]
 	end
-		
+
 	return unpack(buffer)
 end;
 function DirectoryManager.Init(requestedEnvironment)
 	requestedEnvironment = environments[requestedEnvironment] or error('Environment argument is not valid')
 	local internalGetters = DirectoryManager._internalGetters
 	internalGetters[requestedEnvironment.Name] = internalGetters[requestedEnvironment.Name] or {}
-	
+
 	for _, pathWay in ipairs(requestedEnvironment:GetDescendants()) do
 		local derivedResult = nil;
 		if pathWay.ClassName == 'ModuleScript' then
 			derivedResult = DirectoryManager.SafeLoadComponent(pathWay);
 			derivedResult = derivedResult or warn(('Unable to load module: %s'):format(pathWay.Name))
-			
+
 			if type(derivedResult) == 'table' and derivedResult.Init then
 				local _ = ( type(derivedResult.Init) == 'function' ) and derivedResult:Init();
 			end
 		end
 		
-		internalGetters[requestedEnvironment.Name] = derivedResult
+		internalGetters[requestedEnvironment.Name][pathWay.Name] = derivedResult
 	end
+	
 	DirectoryManager._hasNotInitialized[requestedEnvironment.Name] = nil
 	isYielding = isYielding and false
-	
+
 	return true, internalGetters[requestedEnvironment.Name]
 end
 function DirectoryManager.SafeLoadComponent(component)
 	component = ( type(component) == 'userdata' and component ) or error('Component argument must be a userdata')
-	
+
 	local success, result = pcall(require, component)
 	return ( success == true ) and result
 end
